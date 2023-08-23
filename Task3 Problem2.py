89569462b922a7fc626fbf567b9b17f2f6628cd4
lucifer12122 <142308380+lucifer12122@users.noreply.github.com>
2023-08-23 08:58:19 +0530

    Create Task3 Problem2.py

---
+++ Task3 Problem2.py
@@ -0,0 +1,7 @@
+def main():
+    inputs = int(input(" Number of inputs: "))
+    numbers = [int(input(f"Enter number {i+1}: ")) for i in range(inputs)]
+    total = sum(numbers)
+    print(total)
+
+main()
