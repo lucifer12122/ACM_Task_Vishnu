59fe55bdc693fdab4953b8ff4a09d7c014884163
lucifer12122 <142308380+lucifer12122@users.noreply.github.com>
2023-08-25 22:29:29 +0530

    Update Task3 Problem 4.py

--- Task3 Problem 4.py
+++ Task3 Problem 4.py
@@ -1,15 +1,20 @@
 def fibonacci_sequence(n):
     x = [1, 2]
-    while len(x) < n:
+    while x[-1] + x[-2] <= n:
         x.append(x[-1] + x[-2])
     return x
 
+def even_sum(sequence):
+    return sum(filter(lambda x: x % 2 == 0, sequence))
+
 def main():
-    
-        N = int(input())
+        N = int(input("Enter the upper limit for Fibonacci Sum: "))
         sequence = fibonacci_sequence(N)
-        print(sequence)
+        even_numbers = list(filter(lambda x: x % 2 == 0, sequence))
+        result = even_sum(sequence)
+        print(f" Even Fibonacci numbers up to {N}: {even_numbers}. Their sum is {result}.")
 
 if __name__ == "__main__":
     main()
+
 
